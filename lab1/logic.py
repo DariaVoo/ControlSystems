4a087e6de8212b69b4730695861236d951e14bfc
DariaVoo <dasha2003.voo@yandex.ru>
2021-03-18 15:05:45 +0300

    done auto mode

--- lab1/logic.py
+++ lab1/logic.py
@@ -2,6 +2,16 @@ from time import sleep
 
 from parse_json import parse_json
 
+def start_cutting(game_view, request):
+    '''
+    Начинает срезать юниты
+    :return: None
+    '''
+    offset_x = int(request['offset']['x'])
+    offset_y = int(request['offset']['y'])
+    # срезаем юнит = двигаем игрока
+    game_view.on_key_press(offset_x, offset_y)
+    sleep(0.1)
 
 def logic(game_view):
     # game_view.on_key_press(1, 1)
@@ -12,15 +22,22 @@ def logic(game_view):
         request = parse_json()
         print(request)
         sleep(0.1)
-        if request['mode'] == 'AUTO':
-            print('It\'s auto!')
-        elif request['mode'] == 'MANUAL':
+        if request['mode'] == 'MANUAL':
             print('Manual mode')
-            offset_x = int(request['offset']['x'])
-            offset_y = int(request['offset']['y'])
-            # срезаем юнит = двигаем игрока
-            game_view.on_key_press(offset_x, offset_y)
-            sleep(0.1)
+            start_cutting(game_view, request)
             game_view.on_key_release(0)
+        elif request['mode'] == 'AUTO':
+            print('It\'s auto!')
+            if request['control'] == 'IDLE':
+                start_cutting(game_view, request)
+            elif request['control'] == 'PAUSE' or request['control'] == 'STOP':
+                game_view.on_key_release(0)
+                sleep(0.1)
+            elif request['control'] == 'CONTINUE':
+                start_cutting(game_view, request)
+
+
+
+
 
 
--- lab1/parse_json.py
+++ lab1/parse_json.py
@@ -1,7 +1,7 @@
 import json
 
 def parse_json():
-    with open('/home/dvoo/ControlSystems/lab1/request_manual.json', "r") as read_file:
+    with open('/home/dvoo/ControlSystems/lab1/request_auto.json', "r") as read_file:
         data = json.load(read_file)
     return data
 
